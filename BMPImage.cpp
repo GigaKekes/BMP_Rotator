ec0993d154dfbef64cda6881e98d3a67fa40e8d7
Dmitry-klimovski <dimasikklimovski35@gmail.com>
2023-09-24 11:51:28 +0300

    file storage strategy changed added structure

--- BMPImage.cpp
+++ BMPImage.cpp
@@ -1,7 +1,7 @@
 #include "BMPImage.h"
 
-BMPImage::BMPImage(int width, int height):
-	mWidth(width), mHeight(height), mPixels(std::vector<Color>(width*height))
+
+BMPImage::BMPImage(const char* path)
 {
 }
 
@@ -9,17 +9,6 @@ BMPImage::~BMPImage()
 {
 }
 
-Color BMPImage::GetPixel(int row, int col) const
-{
-	return mPixels[col * mWidth + row];
-}
-
-void BMPImage::SetPixel(const Color& color, int row, int col)
-{
-	mPixels[col * mWidth + row].red = color.red;
-	mPixels[col * mWidth + row].green = color.green;
-	mPixels[col * mWidth + row].blue = color.blue;
-}
 
 void BMPImage::RotateIamge()
 {
--- BMPImage.h
+++ BMPImage.h
@@ -1,24 +1,62 @@
-#pragma once
-#include "Color.h"
+#ifndef BMP_IMAGE_H
+#define BMP_IMAGE_H
+
+#define uint unsigned int
+#define uchar unsigned char
+
 #include <vector>
+
+typedef struct BMPHeader
+{
+	uchar id[2];
+	uint fileSize;
+	uchar appSpec[2];
+	uint pixelOffset;
+} BMPHeader;
+
+typedef struct DIBHeader
+{
+	uint DIBHeaderSize;
+	uint width;
+	uint height;
+	uchar plateColorsCount;
+	uchar bitsPerPixel;
+	uint compression;
+	uint dataSize;
+	uint printWidth;
+	uint printHeight;
+	uint paletteColorsCount;
+	uint importantColorsCount;
+
+}DIBHeader;
+
+typedef struct BMPFile
+{
+	BMPHeader bmpHeader;
+	DIBHeader dipHeader;
+	uchar* data;
+}BMPFile;
+
+
 
 class BMPImage
 {
 public:
 	BMPImage(int width, int height);
+	BMPImage(const char* path);
 	~BMPImage();
 
-	Color GetPixel(int row, int col) const;
-	void SetPixel(const Color& color, int row, int col);
-	
 	void RotateIamge();
 	void ApplyGaussianBluring();
 
 	void ExportToFile(const char * path);
 	void ImportFromFile(const char * path);
 
+
 private:
 	int mWidth;
 	int mHeight;
-	std::vector<Color> mPixels;
+	unsigned char* data;
 };
+
+#endif
